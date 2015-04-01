d96d327bc262977d48778204f9773f0fa71a5891
John Hogarty <hogihung@gmail.com>
2015-03-31 20:57:28 -0400

    Initial Commit

---
+++ RockPaperScissorSpockTests/RockPaperScissorSpockTests.swift
@@ -0,0 +1,36 @@
+//
+//  RockPaperScissorSpockTests.swift
+//  RockPaperScissorSpockTests
+//
+//  Created by John F. Hogarty on 3/31/15.
+//  Copyright (c) 2015 John F. Hogarty. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RockPaperScissorSpockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
